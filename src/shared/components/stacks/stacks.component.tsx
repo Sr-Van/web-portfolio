647decd9b67a6f57cfaa5375cda112f50ded41cd
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-07-26 12:25:10 -0300

    feat: stacks component

---
+++ src/shared/components/stacks/stacks.component.tsx
@@ -0,0 +1,25 @@
+import './stacks.component.css';
+import Icon from '../icon';
+
+export const Stacks = () => {
+    return (
+        <div className='w-2/3'>
+            <ul className='flex gap-2 justify-end'>
+                <li title='Angular'>
+                    <Icon name="bxl-angular" size='30px' color='#fff'/>
+                </li>
+                <li title='Typescript'>
+                    <Icon name="bxl-typescript" size='30px' color='#fff'/>
+                </li>
+                
+                <li title='Git'>
+                    <Icon name="bxl-git" size='30px' color='#fff'/>
+                </li>
+
+                <li title='TailwindCSS'>
+                    <Icon name="bxl-tailwind-css" size='30px' color='#fff'/>
+                </li>
+            </ul>
+        </div>
+    );
+}
