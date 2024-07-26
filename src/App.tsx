4bba6fa0148b5207c5ebea51079cd723038b53c1
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-07-26 12:24:04 -0300

    feat: adding main sections and main styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './shared/components/header/header.component';
+import Home from './shared/components/home/home.component';
+import { Projects } from './shared/components/projects/projects.components';
+import { Skills } from './shared/components/skills/skills.component';
+import { Contact } from './shared/components/contact/contact.component';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import 'boxicons';
+
+AOS.init();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App absolute h-full w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      <Header />
+      <Home />
+      <Projects />
+      <Skills />
+      <Contact />
     </div>
   );
 }
