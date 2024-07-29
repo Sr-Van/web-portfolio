87e0caffb4e1433c09af49dd288d48ceb9d92642
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-07-29 07:21:14 -0300

    feat: skill list

--- src/shared/components/skills/skills.component.tsx
+++ src/shared/components/skills/skills.component.tsx
@@ -1,9 +1,13 @@
+import { SkillsList } from "../skillslist.component";
+
 export const Skills = () => {
   return (
-    <section className="h-screen" id="skills">
-        <h1 className="text-2xl text-white text-center flex flex-col items-end h-full w-full bg-slate-950">
+    <section className="min-h-screen flex flex-col items-start w-full bg-slate-950" id="skills">
+        <h1 className="text-2xl text-white text-center py-20 px-10">
             Skills
         </h1>
+
+        <SkillsList />
     </section>
   );
 }
