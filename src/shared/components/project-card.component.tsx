4b2be4f87c717078413e9640888a0ecacce209a3
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-07-29 20:58:14 -0300

    styles: visual fixed

--- src/shared/components/project-card.component.tsx
+++ src/shared/components/project-card.component.tsx
@@ -12,23 +12,36 @@ interface ProjCardProps {
 }
 
 export const ProjCard = (props: ProjCardProps) => {
-  let wait = false
+  let wait = false;
   return (
-    <article 
-    className={`block rounded-md bg-purple-700 w-full h-full max-w-[350px] ${props.isPrimary? 'md:max-w-[1000px]' : ''} shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]`}>
-      <img className={`${'card-' + props.img} rounded-t-md w-full ${props.isPrimary ? 'max-h-[200px] md:max-h-[800px] h-[500px]' : 'h-[200px]'} object-cover`} src={`/img/${props.img}.png`} alt={props.title} 
-      onMouseMove={() => {
-        if(!wait) {          
-          hoverOnImg(props)
-          wait = true
-          setTimeout(() => {
-            wait = false
-          }, 700);
-        }
-      }} />
+    <article
+      className={`block rounded-md bg-gradient-to-tr from-purple-800 to-indigo-700 w-full h-full max-w-[350px] ${
+        props.isPrimary ? "md:max-w-[1000px]" : ""
+      } shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]`}
+    >
+      <img
+        className={`${"card-" + props.img} rounded-t-md w-full ${
+          props.isPrimary
+            ? "max-h-[200px] md:max-h-[800px] h-[500px]"
+            : "h-[200px]"
+        } object-cover`}
+        src={`/img/${props.img}.png`}
+        alt={props.title}
+        onMouseMove={() => {
+          if (!wait) {
+            hoverOnImg(props);
+            wait = true;
+            setTimeout(() => {
+              wait = false;
+            }, 700);
+          }
+        }}
+      />
 
       <div className="infos flex flex-col items-start p-3 h-full max-h-[250px]">
-        <h1 className={`text-xl ${props.isPrimary ? 'munich' : ''}`}>{props.title}</h1>  
+        <h1 className={`text-xl ${props.isPrimary ? "munich" : ""}`}>
+          {props.title}
+        </h1>
         <p className="text-slate-200 text-sm text-start">{props.description}</p>
         <ul className="flex gap-1 mt-3">
           {props.stacks?.map((stack) => (
@@ -38,24 +51,41 @@ export const ProjCard = (props: ProjCardProps) => {
           ))}
         </ul>
 
-        <span className={`flex justify-between items-center w-full ${props.isPrimary ? 'mt-auto' : 'mt-3'}`}>
-          <a 
-          rel="noreferrer" target="_blank" 
-          className={`text-xs text-slate-200 p-3 shadow rounded-lg ${props.isPrimary ? 'md:border-b-4 md:border-slate-800 md:hover:bg-slate-800 md:bg-transparent' : ''} bg-slate-800 hover:bg-slate-700 transition-all`} 
-          href={props.url}>View Project</a>
-          <a rel="noreferrer" target="_blank" href={props.repo} title="Ver repositorio">
+        <span
+          className={`flex justify-between items-center w-full ${
+            props.isPrimary ? "mt-auto" : "mt-3"
+          }`}
+        >
+          <a
+            rel="noreferrer"
+            target="_blank"
+            className={`text-xs text-slate-200 p-3 shadow rounded-lg ${
+              props.isPrimary
+                ? "md:border-b-4 md:border-slate-800 md:hover:bg-slate-800 md:bg-transparent"
+                : ""
+            } bg-slate-800 hover:bg-slate-700 transition-all`}
+            href={props.url}
+          >
+            View Project
+          </a>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href={props.repo}
+            title="Ver repositorio"
+          >
             <Icon name="bxl-github" size="30px" color="#000" />
           </a>
         </span>
       </div>
     </article>
   );
-}
+};
 
 const hoverOnImg = (props: ProjCardProps) => {
   const { img, howManyImgs } = props;
   const randomPick = Math.floor(Math.random() * howManyImgs);
-  let src = `/img/${img}${randomPick === 0 ? '' : randomPick}.png`;
+  let src = `/img/${img}${randomPick === 0 ? "" : randomPick}.png`;
 
-  document.querySelector(`.card-${img}`)?.setAttribute('src', src)
-}
+  document.querySelector(`.card-${img}`)?.setAttribute("src", src);
+};
