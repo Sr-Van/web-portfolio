6a92cd9787fc94f30372e3e5a8d92b0ecf852062
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-08-07 17:56:14 -0300

    feat: floating parallax items

--- src/shared/components/home/home.component.tsx
+++ src/shared/components/home/home.component.tsx
@@ -3,7 +3,7 @@ import { Stacks } from "../stacks/stacks.component";
 export default function Home() {
   return (
     <section
-      className="home flex flex-col gap-2 items-center justify-center h-screen"
+      className="home relative flex flex-col gap-2 items-center justify-center h-screen"
       id="home"
     >
       <div className="w-full flex flex-col md:flex-row md:relative md:w-6/12 lg:w-4/12 xl:w-3/12">
@@ -35,6 +35,56 @@ export default function Home() {
         </figure>
 
         <Stacks />
+      </div>
+
+      <div className="blocks hidden md:block absolute bottom-[90px] left-7 w-[240px] h-[100px] parallax_html">
+        <div className="grid grid-cols-4 grid-rows-3 gap-1">
+          <div
+            data-aos="fade-up"
+            data-aos-delay="400"
+            className="parallax_html_block bg-gradient-to-r from-indigo-500 to-purple-500 h-5 border-4 border-purple-500"
+          ></div>
+          <div
+            data-aos="fade-down"
+            data-aos-delay="400"
+            className="bg-transparent col-span-3"
+          ></div>
+          <div
+            data-aos="fade-right"
+            data-aos-delay="400"
+            className="parallax_html_block bg-gradient-to-r from-indigo-500 to-purple-500 h-5 border-4 border-purple-500"
+          ></div>
+          <div
+            data-aos="fade-left"
+            data-aos-delay="400"
+            className="parallax_html_block bg-gradient-to-r from-indigo-500 to-purple-500 h-5 border-4 border-purple-500"
+          ></div>
+          <div
+            data-aos="fade-up"
+            data-aos-delay="400"
+            className="bg-transparent col-span-2"
+          ></div>
+          <div
+            data-aos="fade-right"
+            data-aos-delay="400"
+            className="parallax_html_block bg-gradient-to-r from-indigo-500 to-purple-500 h-5 border-4 border-purple-500"
+          ></div>
+          <div
+            data-aos="fade-down"
+            data-aos-delay="400"
+            className="parallax_html_block bg-gradient-to-r from-indigo-500 to-purple-500 h-5 border-4 border-purple-500"
+          ></div>
+          <div
+            data-aos="fade-left"
+            data-aos-delay="400"
+            className="parallax_html_block bg-gradient-to-r from-indigo-500 to-purple-500 h-5 border-4 border-purple-500"
+          ></div>
+          <div
+            data-aos="fade-up"
+            data-aos-delay="400"
+            className="bg-transparent"
+          ></div>
+        </div>
       </div>
     </section>
   );
--- src/shared/components/projects/projects.components.tsx
+++ src/shared/components/projects/projects.components.tsx
@@ -3,10 +3,10 @@ import { ProjCard } from "../project-card.component";
 export const Projects = () => {
   return (
     <section
-      className="min-h-screen flex flex-col items-start w-full bg-slate-950"
+      className="min-h-screen relative flex flex-col items-start w-full bg-slate-950 z-20"
       id="projects"
     >
-      <h1 className="py-20 px-10 text-2xl text-white text-center ">Projects</h1>
+      <h1 className="py-20 px-10 text-2xl text-white text-center">Projects</h1>
 
       <div className="wrapper flex justify-center w-full">
         <ul className="flex flex-col md:grid md:grid-cols-2 xl:grid-cols-4 lg:items-start gap-3 items-center w-full md:w-[600px] lg:w-[950px] xl:w-[1000px] px-4">
@@ -93,6 +93,50 @@ export const Projects = () => {
             />
           </li>
         </ul>
+      </div>
+
+      <div
+        data-aos="zoom-in"
+        data-aos-delay="700"
+        className="absolute hidden md:block -top-10 right-4 -rotate-45"
+      >
+        <div className="parallax_css w-40 h-40 bg-gradient-to-r from-slate-800 to-indigo-900 rounded-full relative">
+          <div
+            data-aos="fade-left"
+            data-aos-delay="800"
+            className="w-8 h-8 rounded-full bg-purple-500 absolute bottom-3 right-12"
+          ></div>
+          <div
+            data-aos="fade-left"
+            data-aos-delay="900"
+            className="w-7 h-7 rounded-full bg-yellow-500 absolute bottom-10 right-3"
+          ></div>
+          <div
+            data-aos="fade-left"
+            data-aos-delay="1000"
+            className="w-6 h-6 rounded-full bg-sky-500 absolute bottom-20 right-2"
+          ></div>
+          <div
+            data-aos="fade-left"
+            data-aos-delay="1100"
+            className="w-5 h-5 rounded-full bg-slate-200 absolute top-6 right-8"
+          ></div>
+          <div className="w-20 h-20 rounded-full bg-slate-950 absolute top-[20%] left-3"></div>
+        </div>
+      </div>
+
+      <div
+        data-aos="fade-right"
+        data-aos-delay="2000"
+        className="absolute hidden md:block bottom-32 left-4"
+      >
+        {/* needed for parallax */}
+        <div className="parallax_css ">
+          <div
+            className="w-20 h-20 rounded-full shadow bg-gradient-to-r from-slate-800 via-blue-500 to-indigo-900 animate-pulse cursor-pointer hover:animate-none bg-size-200 bg-pos-0 hover:bg-pos-100 transition-all"
+            title="click me :)"
+          ></div>
+        </div>
       </div>
     </section>
   );
