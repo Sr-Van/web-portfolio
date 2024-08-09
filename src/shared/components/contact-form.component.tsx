c1b3a37e36f417aac3f89c59919d63b4dd41f8d4
Vanderson Erlan <vandersonerlan@Vandersons-MBP.bbrouter>
2024-08-09 14:05:00 -0300

    fix: removing unused form

--- src/shared/components/contact-form.component.tsx
+++ src/shared/components/contact-form.component.tsx
@@ -6,36 +6,17 @@ export const ContactForm = () => {
       data-aos-delay="700"
     >
       <h2 className="text-2xl text-white text-center">Send me a message!</h2>
-      <form className="w-full max-w-[500px]">
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          required
-          className="w-full p-1 mb-3 text-black placeholder-slate-600 rounded bg-slate-300 border-2 border-transparent focus-within:border-purple-500 outline-none transition-all"
-        />
-        <input
-          type="email"
-          name="Email"
-          placeholder="Email"
-          aria-label="Email"
-          required
-          className="w-full p-1 mb-3 text-black placeholder-slate-600 rounded bg-slate-300 border-2 border-transparent focus-within:border-purple-500 outline-none transition-all"
-        />
-        <textarea
-          name="Message"
-          placeholder="Message"
-          aria-label="Message"
-          required
-          className="w-full p-1 mb-3 text-black placeholder-slate-600 rounded bg-slate-300 border-2 border-transparent focus-within:border-purple-500 outline-none transition-all"
-        />
+      <form
+        className="w-full max-w-[500px]"
+        action="mailto:contato@vanderson-dev.com"
+      >
         <button
           type="submit"
           aria-label="Send message"
           className="w-full p-1 mb-3 rounded bg-gradient-to-r from-indigo-500 to-purple-500 text-white shadow-md transition-al border-2 border-purple-500/60"
           onClick={() => sendForm(document.forms[0])}
         >
-          Enviar
+          Send
         </button>
       </form>
     </div>
